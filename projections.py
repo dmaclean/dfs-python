5ff50b07f9d30d2a3fe6817283b37983aa374ed5
Dan MacLean <dan.maclean@agencyport.com>
2013-12-01 08:22:54 -0500

    Added functions for getting player info and league average for various stats.

--- projections.py
+++ projections.py
@@ -1,15 +1,41 @@
 import sys
 import mysql.connector
 
+from datetime import date
+
 cnx = mysql.connector.connect(user='fantasy', password='fantasy', host='localhost', database='basketball_reference')
 
-def get_team(player_id, season):
+league_averages = {}
+
+def get_player_info(player_id):
+	cursor = cnx.cursor()
+	query = """
+		select * from players p where id = '%s'
+	""" % (player_id)
+	
+	try:
+		cursor.execute(query)
+		info = {}
+		for result in cursor:
+			info["id"] = result[0]
+			info["name"] = result[1]
+			info["position"] = result[2]
+			info["height"] = result[3]
+			info["weight"] = result[4]
+			info["url"] = result[5]
+	finally:
+		cursor.close()
+		
+	return info
+
+def get_team(player_id, season, date=date.today()):
 	cursor = cnx.cursor()
 	
 	query = """
 		select team from game_totals_basic where player_id = '%s' and season = %d and
-			game_number = (select max(game_number) from game_totals_basic where player_id = '%s' and season = %d)
-	""" % (player_id, season, player_id, season)
+			game_number = (select max(game_number) from game_totals_basic 
+				where player_id = '%s' and season = %d and date <= '%s')
+	""" % (player_id, season, player_id, season, date)
 	
 	try:
 		cursor.execute(query)
@@ -87,13 +113,58 @@ def calculate_pace(team, season):
 	finally:
 		cursor.close()
 
-def calculate_defense_factor_vs_position(position, team, season):
+########################################################
+# Calculates the league average for the provided stat.
+########################################################
+def calculate_league_avg(stat, position, season):
+	##########################################################
+	# This calculation is somewhat expensive, so just return
+	# the cached value if we've already computed it.
+	##########################################################
+	key = stat + "-" + position
+	if key in league_averages:
+		return league_averages[key]
+	
 	cursor = cnx.cursor()
 	query = """
-		select sum(b.points)/(select max(game) from team_game_totals where team = '%s' and season = %d) 
-		from players p inner join game_totals_basic b on p.id = b.player_id 
-		where b.season = %d and p.position = '%s' and b.opponent = '%s'
-	""" % (team, season, season, position, team)
+		select sum(%s)/(select max(t.game) from team_game_totals t where t.team = b.opponent and season = %d) as "avg", opponent 
+		from game_totals_basic b inner join players p on p.id = b.player_id 
+		where season = %d and position = '%s'
+		group by opponent 
+		order by avg desc
+	""" % (stat, season, season, position)
+	
+	total = -1
+	count = 0
+	try:
+		cursor.execute(query)
+		for result in cursor:
+			count = count + 1
+			total = total + result[0]
+	finally:
+		cursor.close()
+	
+	avg = total/count
+	league_averages[key] = avg
+	
+	return avg
+
+def calculate_defense_factor_vs_position(position, team, season, league_avg = False):
+	cursor = cnx.cursor()
+	query = ""
+	if league_avg:
+		query = """
+			select sum(b.points)/(select max(game) from team_game_totals where season = %d) 
+			from players p inner join game_totals_basic b on p.id = b.player_id 
+			where b.season = %d and p.position = '%s' and b.opponent = '%s'
+		""" % (team, season, season, position, team)
+	else:
+		query = """
+			select sum(b.points)/(select max(game) from team_game_totals where team = '%s' and season = %d) 
+			from players p inner join game_totals_basic b on p.id = b.player_id 
+			where b.season = %d and p.position = '%s' and b.opponent = '%s'
+		""" % (team, season, season, position, team)
+	
 	
 	factor = -1
 	try:
@@ -137,7 +208,10 @@ def get_baseline(player_id, season, stat):
 	
 	return (avg_stat, avg_usage_pct, avg_off_rating, avg_def_rating)
 
-def calculate_projection(player_id, season, opponent):
+def calculate_projection(player_id, season, opponent, date=date.today()):
+	print "%s" % date
+
+	info = get_player_info(player_id)
 	team = get_team(player_id, season)
 	baselines = get_baseline(id,2013,'points')
 	
@@ -154,10 +228,14 @@ def calculate_projection(player_id, season, opponent):
 	
 	adjusted_points = float(avg_points) * float(pace_factor)
 	
-	######################################
-	# Effectiveness of opponent defense.
-	######################################
+	######################################################################
+	# Effectiveness of opponent defense, compared to the league average
+	# for this player's position.
+	######################################################################
+	league_avg = calculate_league_avg("points", info["position"], season)
+	def_factor = calculate_defense_vs_position("points", info["position"], opponent, season)
 	
+	adjusted_points = adjusted_points * float(def_factor/league_avg)
 	
 	return adjusted_points
 
@@ -165,4 +243,5 @@ positions = ["G","F","C"]
 teams = ["ATL","BOS","BRK","LAL"]
 
 id = 'anthoca01'
-print calculate_projection(id, 2013, 'BOS')
+game_date = date(2013,11,25)
+print calculate_projection(id, 2013, 'BOS', game_date)
