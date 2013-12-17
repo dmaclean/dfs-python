dd0ccd3e93f31bee892564bee5982236b80ea8f7
Dan MacLean <dan.maclean@agencyport.com>
2013-12-17 12:44:46 -0500

    Refactored script to compute fantasy points into a class, renamed it FantasyPointCalculator, and added unit tests for it.

---
+++ fantasy_point_calculator.py
@@ -0,0 +1,99 @@
+import sys
+import mysql.connector
+
+class FantasyPointCalculator():
+	DRAFT_DAY = "DRAFT_DAY"
+	DRAFT_KINGS = "DRAFT_KINGS"
+
+	def __init__(self, cnx = None, site=None):
+		self.site = site
+		
+		# Use dependency injection to determine where the database connection comes from.
+		if(not cnx):
+			self.cnx = mysql.connector.connect(user='fantasy', password='fantasy', host='localhost', database='basketball_reference')
+		else:
+			self.cnx = cnx
+
+	def calculate(self, stats):
+		fantasy_points = 0
+	
+		if self.site == self.DRAFT_DAY:
+			missed_shots = (stats["field_goal_attempts"] - stats["field_goals"]) + (stats["three_point_field_goal_attempts"] - stats["three_point_field_goals"]) + (stats["free_throw_attempts"] - stats["free_throws"])
+			
+			fantasy_points = stats["points"] + (stats["total_rebounds"] * 1.25) + (stats["assists"] * 1.5) + (stats["steals"] * 2) + (stats["blocks"] * 2) - (stats["turnovers"] * 1) - (missed_shots * 0.25)
+			
+			# 1 point bonus for 3 pointer made
+			fantasy_points = fantasy_points + stats["three_point_field_goals"]
+
+			triple_or_double_double = 0
+			criteria = [stats["points"], stats["total_rebounds"], stats["assists"], stats["steals"], stats["blocks"]]
+			for c in criteria:
+				if c >= 10:
+					triple_or_double_double = triple_or_double_double + 1
+
+			if triple_or_double_double == 2:
+				fantasy_points = fantasy_points + 2
+			elif triple_or_double_double == 3:
+				fantasy_points = fantasy_points + 2
+		
+		return fantasy_points
+	
+	def run(self):
+		self.site = DRAFT_DAY
+		for arg in sys.argv:
+			if arg == "fantasy_point_calculator.py":
+				pass
+			else:
+				pieces = arg.split("=")
+				if pieces[0] == "site":
+					site = pieces[1]
+
+
+		cnx = mysql.connector.connect(user='fantasy', password='fantasy', host='localhost', database='basketball_reference')
+		cnx_insert = mysql.connector.connect(user='fantasy', password='fantasy', host='localhost', database='basketball_reference')
+
+		cursor = cnx.cursor()
+		query = ("Select * from game_totals_basic t left join fantasy_points p on t.id = p.id where p.id is NULL;")
+		cursor.execute(query)
+
+		for (result) in cursor:
+			player_id = result[1]
+			season = result[2]
+			game_number = result[3]
+			field_goals = result[12]
+			field_goal_attempts = result[13]
+			three_point_field_goals = result[15]
+			three_point_field_goal_attempts = result[16]
+			free_throws = result[18]
+			free_throw_attempts = result[19]
+			total_rebounds = result[23]
+			assists = result[24]
+			steals = result[25]
+			blocks = result[26]
+			turnovers = result[27]
+			points = result[29]
+	
+			missed_shots = (field_goal_attempts - field_goals) + (three_point_field_goal_attempts - three_point_field_goals) + (free_throw_attempts - free_throws)
+
+			fantasy_points = points + (total_rebounds * 1.25) + (assists * 1.5) + (steals * 2) + (blocks * 2) - (turnovers * 0.5) - (missed_shots * 0.25)
+	
+			# 1 point bonus for 3 pointer made
+			fantasy_points = fantasy_points + three_point_field_goals
+	
+			triple_or_double_double = 0
+			criteria = [points, total_rebounds, assists, steals, blocks]
+			for c in criteria:
+				if c >= 10:
+					triple_or_double_double = triple_or_double_double + 1
+
+			if triple_or_double_double == 2:
+				fantasy_points = fantasy_points + 1
+			elif triple_or_double_double == 3:
+				fantasy_points = fantasy_points + 2
+	
+			cursor2 = cnx_insert.cursor()
+			insert_query = ("insert into fantasy_points (player_id, site, season, game_number, points) values ('%s','%s',%d,%d,%f)") % (player_id, site, season, game_number, fantasy_points)
+			cursor2.execute(insert_query)
+			cursor2.close()
+	
+		cursor.close()
---
+++ unittest/test_fantasy_point_calculator.py
@@ -0,0 +1,288 @@
+import sys
+sys.path.append('..')
+
+from datetime import date
+import sqlite3
+import unittest
+import BBRTestUtility
+import fantasy_point_calculator
+
+class TestFantasyPointCalculator(unittest.TestCase):
+	def setUp(self):
+		self.testUtil = BBRTestUtility.BBRTestUtility()
+		self.fpc = fantasy_point_calculator.FantasyPointCalculator(self.testUtil.conn)
+		self.testUtil.runSQL()
+		
+		# Initialize the player info map.
+		self.player_info = {
+			"id": "",
+			"name": "Test",
+			"position": "G",
+			"height": 0,
+			"weight": 0,
+			"url": "something"
+		}
+		
+		# Initialize the player game totals map.
+		self.game_totals_basic_info = {
+			"player_id": "test",
+			"season": date.today().year,
+			"game_number": 0,
+			"date": date.today(),
+			"age": 0,
+			"team": "BOS",
+			"home": True,
+			"opponent": "BOS",
+			"result": "",
+			"games_started": 0,
+			"minutes_played": 0,
+			"field_goals": 0,
+			"field_goal_attempts": 0,
+			"field_goal_pct": 0,
+			"three_point_field_goals": 0,
+			"three_point_field_goal_attempts": 0,
+			"three_point_field_goal_pct": 0,
+			"free_throws": 0,
+			"free_throw_attempts": 0,
+			"free_throw_pct": 0,
+			"offensive_rebounds": 0,
+			"defensive_rebounds": 0,
+			"total_rebounds": 0,
+			"assists": 0,
+			"steals": 0,
+			"blocks": 0,
+			"turnovers": 0,
+			"personal_fouls": 0,
+			"points": 0,
+			"game_score": 0,
+			"plus_minus": 0
+		}
+		
+		self.game_totals_advanced_info = {
+			"player_id": "",
+			"game_number": 0,
+			"season": date.today().year,
+			"date": date.today(),
+			"age": 0,
+			"team": "BOS",
+			"home": True,
+			"opponent": "NYK",
+			"result": "",
+			"games_started": 0,
+			"minutes_played": 0,
+			"true_shooting_pct": 0,
+			"effective_field_goal_pct": 0,
+			"offensive_rebound_pct": 0,
+			"defensive_rebound_pct": 0,
+			"total_rebound_pct": 0,
+			"assist_pct": 0,
+			"steal_pct": 0,
+			"block_pct": 0,
+			"turnover_pct": 0,
+			"usage_pct": 0,
+			"offensive_rating": 0,
+			"defensive_rating": 0,
+			"game_score": 0
+		}
+		
+		# Initialize the team game totals map.
+		self.team_game_totals_info = {
+			"team": "",
+			"season": date.today().year,
+			"game": 0,
+			"date": date.today(),
+			"home": True,
+			"opponent": "",
+			"result": "",
+			"minutes_played": 240,
+			"field_goals": 0,
+			"field_goal_attempts": 0,
+			"three_point_field_goals": 0,
+			"three_point_field_goal_attempts": 0,
+			"free_throws": 0,
+			"free_throw_attempts": 0,
+			"offensive_rebounds": 0,
+			"total_rebounds": 0,
+			"assists": 0,
+			"steals": 0,
+			"blocks": 0,
+			"turnovers": 0,
+			"personal_fouls": 0,
+			"points": 0,
+			"opp_field_goals": 0,
+			"opp_field_goal_attempts": 0,
+			"opp_three_point_field_goals": 0,
+			"opp_three_point_field_goal_attempts": 0,
+			"opp_free_throws": 0,
+			"opp_free_throw_attempts": 0,
+			"opp_offensive_rebounds": 0,
+			"opp_total_rebounds": 0,
+			"opp_assists": 0,
+			"opp_steals": 0,
+			"opp_blocks": 0,
+			"opp_turnovers": 0,
+			"opp_personal_fouls": 0,
+			"opp_points": 0
+		}
+		
+		self.schedule_info = {
+			"date": date.today(),
+			"season": date.today().year,
+			"visitor": "",
+			"home": ""
+		}
+	
+	def tearDown(self):
+		self.testUtil.conn.close()
+		
+		self.player_info = {}
+	
+	##################################################
+	# DraftDay - player with no double/triple-double
+	#
+	# 8/10 field goals
+	# 1/2 three pointers
+	# 5/6 free throws
+	# 5 rebounds
+	# 2 assists
+	# 1 steal
+	# 2 blocks
+	# 1 turnover
+	# 24 points
+	#
+	# 24 fantasy points from points
+	# 1 on 3pt bonus
+	# -1 on 4 missed shots
+	# 6.25 on rebounds
+	# 3 on assists
+	# 2 on steals
+	# 4 on blocks
+	# -1 on turnovers
+	# 0 for double-double
+	# 0 for triple-double
+	# 38.25 total fantasy points
+	##################################################
+	def test_calculate_draft_day(self):
+		stats = {
+			"player_id": "dmaclean",
+			"season": date.today(),
+			"game_number": 1,
+			"field_goals": 8,
+			"field_goal_attempts": 10,
+			"three_point_field_goals": 1,
+			"three_point_field_goal_attempts": 2,
+			"free_throws": 5,
+			"free_throw_attempts": 6,
+			"total_rebounds": 5,
+			"assists": 2,
+			"steals": 1,
+			"blocks": 2,
+			"turnovers": 1,
+			"points": 24
+		}
+		
+		self.fpc.site = self.fpc.DRAFT_DAY
+		points = self.fpc.calculate(stats)
+		
+		self.assertTrue(points == 38.25)
+	
+	##################################################
+	# DraftDay - player with double-double
+	#
+	# 8/10 field goals
+	# 1/2 three pointers
+	# 5/6 free throws
+	# 10 rebounds
+	# 2 assists
+	# 1 steal
+	# 2 blocks
+	# 1 turnover
+	# 24 points
+	#
+	# 24 fantasy points from points
+	# 1 on 3pt bonus
+	# -1 on 4 missed shots
+	# 12.5 on rebounds
+	# 3 on assists
+	# 2 on steals
+	# 4 on blocks
+	# -1 on turnovers
+	# 2 for double-double
+	# 0 for triple-double
+	# 46.5 total fantasy points
+	##################################################
+	def test_calculate_draft_day_double_double(self):
+		stats = {
+			"player_id": "dmaclean",
+			"season": date.today(),
+			"game_number": 1,
+			"field_goals": 8,
+			"field_goal_attempts": 10,
+			"three_point_field_goals": 1,
+			"three_point_field_goal_attempts": 2,
+			"free_throws": 5,
+			"free_throw_attempts": 6,
+			"total_rebounds": 10,
+			"assists": 2,
+			"steals": 1,
+			"blocks": 2,
+			"turnovers": 1,
+			"points": 24
+		}
+		
+		self.fpc.site = self.fpc.DRAFT_DAY
+		points = self.fpc.calculate(stats)
+		
+		self.assertTrue(points == 46.5)
+	
+	##################################################
+	# DraftDay - player with triple-double
+	#
+	# 8/10 field goals
+	# 1/2 three pointers
+	# 5/6 free throws
+	# 10 rebounds
+	# 10 assists
+	# 1 steal
+	# 2 blocks
+	# 1 turnover
+	# 24 points
+	#
+	# 24 fantasy points from points
+	# 1 on 3pt bonus
+	# -1 on 4 missed shots
+	# 12.5 on rebounds
+	# 15 on assists
+	# 2 on steals
+	# 4 on blocks
+	# -1 on turnovers
+	# 0 for double-double
+	# 2 for triple-double
+	# 58.5 total fantasy points
+	##################################################
+	def test_calculate_draft_day_triple_double(self):
+		stats = {
+			"player_id": "dmaclean",
+			"season": date.today(),
+			"game_number": 1,
+			"field_goals": 8,
+			"field_goal_attempts": 10,
+			"three_point_field_goals": 1,
+			"three_point_field_goal_attempts": 2,
+			"free_throws": 5,
+			"free_throw_attempts": 6,
+			"total_rebounds": 10,
+			"assists": 10,
+			"steals": 1,
+			"blocks": 2,
+			"turnovers": 1,
+			"points": 24
+		}
+		
+		self.fpc.site = self.fpc.DRAFT_DAY
+		points = self.fpc.calculate(stats)
+		
+		self.assertTrue(points == 58.5)
+	
+if __name__ == '__main__':
+	unittest.main()
