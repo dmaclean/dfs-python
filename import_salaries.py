af0ece1ffa14bbcf6b163969c6d330e5f49d6645
Dan MacLean <dan.maclean@agencyport.com>
2013-12-19 22:03:46 -0500

    Converted the salary importer to be a class, and added unit tests.

--- import_salaries.py
+++ import_salaries.py
@@ -2,81 +2,109 @@ import sys
 from datetime import date
 import mysql.connector
 
-def insert_or_update_positions(player_id, site, positions, conn):
-	cursor = conn.cursor()
+class SalaryImporter:
 	
-	query = "select id from dfs_site_positions where player_id = '%s' and site = '%s' limit 1" % (player_id, site)
-	
-	try:
-		cursor.execute(query)
+	def __init__(self, conn=None):
+		if conn:
+			self.cnx = conn
+		else:
+			self.cnx = mysql.connector.connect(user='fantasy', password='fantasy', host='localhost', database='basketball_reference')
 		
-		# We found a result, so do an update
-		for result in cursor:
-			query = "update dfs_site_positions set position = '%s' where player_id = '%s' and site = '%s'" % (positions, player_id, site)
+		self.site = "DRAFT_DAY"
+		self.salaries = {}
+		self.positions = {}
+	
+	#########################################################################################
+	# Maintains player positions for each site.  We want to keep track of which players are
+	# allowed at which positions for a given site.  This will allow for more accuracy when
+	# automating roster generation.
+	#########################################################################################
+	def insert_or_update_positions(self, player_id, site, positions):
+		cursor = self.cnx.cursor()
+	
+		query = "select id from dfs_site_positions where player_id = '%s' and site = '%s' limit 1" % (player_id, site)
+	
+		try:
 			cursor.execute(query)
-			return
 		
-		# No result, do an insert
-		query = "insert into dfs_site_positions (player_id, site, position) values ('%s','%s','%s')" % (player_id, site, positions)
-		cursor.execute(query)
-	finally:
-		cursor.close()
+			# We found a result, so do an update
+			for result in cursor:
+				query = "update dfs_site_positions set position = '%s' where player_id = '%s' and site = '%s'" % (positions, player_id, site)
+				cursor.execute(query)
+				return
+		
+			# No result, do an insert
+			query = "insert into dfs_site_positions (player_id, site, position) values ('%s','%s','%s')" % (player_id, site, positions)
+			cursor.execute(query)
+		finally:
+			cursor.close()
 
-cnx = mysql.connector.connect(user='fantasy', password='fantasy', host='localhost', database='basketball_reference')
+	#######################################################################################
+	# Parses the provided line to properly extract the player name, position, and salary.
+	# The position and salary are both stored in dictionaries which are keyed off the
+	# player name.
+	#######################################################################################
+	def process_line(self, line):
+		pieces = line.split(",")
+		if self.site == "DRAFT_DAY":
+			position = pieces[0].replace("\"", "")
+			name = pieces[1].replace("\"", "").replace("'","")
+			salary = int(pieces[4])
+		elif self.site == "DRAFT_KINGS":
+			position = pieces[0].replace("\"", "")
+			name = pieces[1].replace("\"", "").replace("'","")
+			salary = int(pieces[2])
+		elif self.site == "STAR_STREET":
+			position = pieces[0].replace("\"", "").replace("-","/")
+			name = pieces[1].replace("\"", "").replace("'","")
+			salary = int(pieces[6])
 
-site = "DRAFT_DAY"
-file = ""
+		self.salaries[name] = salary
+		if name in self.positions:
+			self.positions[name] = self.positions[name] + "/" + position
+		else:
+			self.positions[name] = position
 
-for arg in sys.argv:
-	if arg == "import_salaries.py":
-		pass
-	else:
-		pieces = arg.split("=")
-		if pieces[0] == "site":
-			site = pieces[1]
-		elif pieces[0] == "file":
-			file = pieces[1]
+	def run(self):
+		file = ""
 
-f = open(file, "r")
-read_header = False
-salaries = {}
-positions = {}
+		for arg in sys.argv:
+			if arg == "import_salaries.py":
+				pass
+			else:
+				pieces = arg.split("=")
+				if pieces[0] == "site":
+					self.site = pieces[1]
+				elif pieces[0] == "file":
+					file = pieces[1]
 
-print "Importing salaries for %s" % site
+		f = open(file, "r")
+		read_header = False
+		
 
-for line in f:
-	if not read_header:
-		read_header = True
-		continue
-	pieces = line.split(",")
-	if site == "DRAFT_DAY":
-		position = pieces[0].replace("\"", "")
-		name = pieces[1]
-		salary = int(pieces[4])
-	elif site == "DRAFT_KINGS":
-		position = pieces[0].replace("\"", "")
-		name = pieces[1]
-		salary = int(pieces[2])
-	elif site == "STAR_STREET":
-		position = pieces[0].replace("\"", "").replace("-","/")
-		name = pieces[1]
-		salary = int(pieces[6])
+		print "Importing salaries for %s" % self.site
+
+		for line in f:
+			if not read_header:
+				read_header = True
+				continue
+			self.process_line(line)
+
+		for k in self.salaries:
+			cursor = self.cnx.cursor()
+			query = ("select id from players where name = '%s'") % (k.replace("'","").replace("\"",""))
+			cursor.execute(query)
 	
-	salaries[name] = salary
-	positions[name] = position
+			player_id = ""
+			for (id) in cursor:
+				player_id = id[0]
+				query = ("insert into salaries (player_id, site, salary, date) values ('%s','%s',%d, '%s')") % (player_id,self.site,self.salaries[k],date.today())
+				cursor.execute(query)
+	
+			cursor.close()
+	
+			self.insert_or_update_positions(player_id, self.site, self.positions[k])
 
-for k in salaries:
-	cursor = cnx.cursor()
-	query = ("select id from players where name = '%s'") % (k.replace("'","").replace("\"",""))
-	cursor.execute(query)
-	
-	player_id = ""
-	for (id) in cursor:
-		player_id = id[0]
-		query = ("insert into salaries (player_id, site, salary, date) values ('%s','%s',%d, '%s')") % (player_id,site,salaries[k],date.today())
-		cursor.execute(query)
-	
-	cursor.close()
-	
-	insert_or_update_positions(player_id, site, positions[k], cnx)
-
+if __name__ == '__main__':
+	sa = SalaryImporter()
+	sa.run()
---
+++ unittest/test_import_salaries.py
@@ -0,0 +1,176 @@
+import sys
+sys.path.append('..')
+
+from datetime import date
+import sqlite3
+import unittest
+import BBRTestUtility
+import import_salaries
+
+class TestSalaryImporter(unittest.TestCase):
+	def setUp(self):
+		self.testUtil = BBRTestUtility.BBRTestUtility()
+		self.salary_importer = import_salaries.SalaryImporter(self.testUtil.conn)
+		self.testUtil.runSQL()
+		
+		# Initialize the player info map.
+		self.player_info = {
+			"id": "",
+			"name": "Test",
+			"position": "G",
+			"height": 0,
+			"weight": 0,
+			"url": "something"
+		}
+		
+		# Initialize the player game totals map.
+		self.game_totals_basic_info = {
+			"player_id": "test",
+			"season": date.today().year,
+			"game_number": 0,
+			"date": date.today(),
+			"age": 0,
+			"team": "BOS",
+			"home": True,
+			"opponent": "BOS",
+			"result": "",
+			"games_started": 0,
+			"minutes_played": 0,
+			"field_goals": 0,
+			"field_goal_attempts": 0,
+			"field_goal_pct": 0,
+			"three_point_field_goals": 0,
+			"three_point_field_goal_attempts": 0,
+			"three_point_field_goal_pct": 0,
+			"free_throws": 0,
+			"free_throw_attempts": 0,
+			"free_throw_pct": 0,
+			"offensive_rebounds": 0,
+			"defensive_rebounds": 0,
+			"total_rebounds": 0,
+			"assists": 0,
+			"steals": 0,
+			"blocks": 0,
+			"turnovers": 0,
+			"personal_fouls": 0,
+			"points": 0,
+			"game_score": 0,
+			"plus_minus": 0
+		}
+		
+		self.game_totals_advanced_info = {
+			"player_id": "",
+			"game_number": 0,
+			"season": date.today().year,
+			"date": date.today(),
+			"age": 0,
+			"team": "BOS",
+			"home": True,
+			"opponent": "NYK",
+			"result": "",
+			"games_started": 0,
+			"minutes_played": 0,
+			"true_shooting_pct": 0,
+			"effective_field_goal_pct": 0,
+			"offensive_rebound_pct": 0,
+			"defensive_rebound_pct": 0,
+			"total_rebound_pct": 0,
+			"assist_pct": 0,
+			"steal_pct": 0,
+			"block_pct": 0,
+			"turnover_pct": 0,
+			"usage_pct": 0,
+			"offensive_rating": 0,
+			"defensive_rating": 0,
+			"game_score": 0
+		}
+		
+		# Initialize the team game totals map.
+		self.team_game_totals_info = {
+			"team": "",
+			"season": date.today().year,
+			"game": 0,
+			"date": date.today(),
+			"home": True,
+			"opponent": "",
+			"result": "",
+			"minutes_played": 240,
+			"field_goals": 0,
+			"field_goal_attempts": 0,
+			"three_point_field_goals": 0,
+			"three_point_field_goal_attempts": 0,
+			"free_throws": 0,
+			"free_throw_attempts": 0,
+			"offensive_rebounds": 0,
+			"total_rebounds": 0,
+			"assists": 0,
+			"steals": 0,
+			"blocks": 0,
+			"turnovers": 0,
+			"personal_fouls": 0,
+			"points": 0,
+			"opp_field_goals": 0,
+			"opp_field_goal_attempts": 0,
+			"opp_three_point_field_goals": 0,
+			"opp_three_point_field_goal_attempts": 0,
+			"opp_free_throws": 0,
+			"opp_free_throw_attempts": 0,
+			"opp_offensive_rebounds": 0,
+			"opp_total_rebounds": 0,
+			"opp_assists": 0,
+			"opp_steals": 0,
+			"opp_blocks": 0,
+			"opp_turnovers": 0,
+			"opp_personal_fouls": 0,
+			"opp_points": 0
+		}
+		
+		self.schedule_info = {
+			"date": date.today(),
+			"season": date.today().year,
+			"visitor": "",
+			"home": ""
+		}
+		
+		self.dfs_site_positions_info = {
+			"player_id": "",
+			"site": "",
+			"position": ""
+		}
+	
+	def tearDown(self):
+		self.testUtil.conn.close()
+		
+		self.player_info = {}
+	
+	def test_insert_or_update_positions(self):
+		self.salary_importer.insert_or_update_positions("macleda01", "DRAFT_KINGS", "PG")
+		self.assertTrue(self.testUtil.select_from_dfs_site_positions("macleda01", "DRAFT_KINGS") == "PG")
+		
+		self.salary_importer.insert_or_update_positions("macleda01", "DRAFT_KINGS", "SF")
+		self.assertTrue(self.testUtil.select_from_dfs_site_positions("macleda01", "DRAFT_KINGS") == "SF")
+		
+	def test_process_line_draft_day(self):
+		self.salary_importer.site = "DRAFT_DAY"
+		self.salary_importer.process_line("C,Elton Brand,Hawks,Kings,5000,9.2")
+		self.salary_importer.process_line("PF,Elton Brand,Hawks,Kings,5000,9.2")
+		
+		self.assertTrue(self.salary_importer.salaries["Elton Brand"] == 5000)
+		self.assertTrue(self.salary_importer.positions["Elton Brand"] == "C/PF")
+	
+	def test_process_line_draft_kings(self):
+		self.salary_importer.site = "DRAFT_KINGS"
+		self.salary_importer.process_line("\"PF/C\",\"Kevin Love\",10800,\"Por@Min 08:00PM ET\",51.802")
+		
+		self.assertTrue(self.salary_importer.salaries["Kevin Love"] == 10800)
+		self.assertTrue(self.salary_importer.positions["Kevin Love"] == "PF/C")
+	
+	def test_process_line_star_street(self):
+		self.salary_importer.site = "STAR_STREET"
+		self.salary_importer.process_line("PF-C,Kevin Love,MIN,MIN vs POR, 8:00 PM,47.9,18000,\"\",")
+		
+		self.assertTrue(self.salary_importer.salaries["Kevin Love"] == 18000)
+		self.assertTrue(self.salary_importer.positions["Kevin Love"] == "PF/C")
+	
+if __name__ == '__main__':
+	unittest.main()
