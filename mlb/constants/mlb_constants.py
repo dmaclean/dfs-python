df8b0c017a8cf768504cc60af803c78593e6f390
Dan MacLean <dmaclean82@gmail.com>
2014-03-31 13:38:03 -0400

    Added constants for "Player Value--Pitching" table

--- mlb/constants/mlb_constants.py
+++ mlb/constants/mlb_constants.py
@@ -41,3 +41,24 @@ class MLBConstants:
     WALKS_PER_9_INNINGS = "walks_per_9_innings"
     STRIKE_OUTS_PER_9_INNINGS = "strike_outs_per_9_innings"
     STRIKE_OUT_TO_WALK_RATIO = "strike_out_to_walk_ratio"
+
+    ###########################
+    # Player Value - Pitching
+    ###########################
+    PLAYER_VALUE_PITCHING = "player_value_pitching"
+    RUNS_ALLOWED_PER_9_INNINGS = "runs_allowed_per_9_innings"
+    RUNS_ALLOWED_PER_9_INNINGS_OPP = "runs_allowed_per_9_innings_by_opponent"
+    RUNS_PER_9_INNINGS_IN_SUPPORT_FROM_DEFENSE = "runs_allowed_per_9_innings_in_support_from_defense"
+    RUNS_PER_9_INNINGS_BY_ROLE = "runs_per_9_innings_by_role"
+    PARK_FACTORS = "park_factors"
+    RUNS_PER_9_INNINGS_FOR_AVG_PITCHER = "runs_per_9_innings_for_avg_pitcher"
+    RUNS_BETTER_THAN_AVG = "runs_better_than_avg"
+    WINS_ABOVE_AVG = "wins_above_avg"
+    GAME_ENTERING_LEVERAGE_INDEX = "game_entering_leverage_index"
+    WINS_ABOVE_AVG_ADJUSTMENT = "wins_above_average_adjustment"
+    WINS_ABOVE_REPLACEMENT = "wins_above_replacement"
+    RUNS_BETTER_THAN_REPLACEMENT = "runs_better_than_replacement"
+    WIN_LOSS_PCT_WITH_AVG_TEAM = "win_loss_pct_with_avg_team"
+    WIN_LOSS_PCT_WITH_AVG_TEAM_SEASON = "win_loss_pct_with_avg_team_season"
+    SALARY = "salary"
+
