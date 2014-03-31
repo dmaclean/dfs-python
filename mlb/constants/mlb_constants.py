f27a2504f40562b710d8a17dbf3b81746930a0b7
Dan MacLean <dmaclean82@gmail.com>
2014-03-31 08:29:54 -0400

    Added a constants class

---
+++ mlb/constants/mlb_constants.py
@@ -0,0 +1,43 @@
+__author__ = 'dan'
+
+class MLBConstants:
+
+    def __init__(self):
+        pass
+
+    ##############################
+    # Stats in Standard Pitching
+    ##############################
+    STANDARD_PITCHING = "standard_pitching"
+    AGE = "age"
+    TEAM = "team"
+    LEAGUE = "league"
+    WINS = "wins"
+    LOSSES = "losses"
+    WIN_LOSS_PCT = "win_loss_pct"
+    ERA = "era"
+    GAMES = "games"
+    GAMES_STARTED = "games_started"
+    GAMES_FINISHED = "games_finished"
+    COMPLETE_GAMES = "complete_games"
+    SHUT_OUTS = "shut_outs"
+    SAVES = "saves"
+    INNINGS_PITCHED = "innings_pitched"
+    HITS = "hits"
+    RUNS = "runs"
+    EARNED_RUNS = "earned_runs"
+    HOME_RUNS = "home_runs"
+    WALKS = "walks"
+    INTENTIONAL_WALKS = "intentional_walks"
+    STRIKE_OUTS = "strike_outs"
+    HIT_BY_PITCH = "hit_by_pitch"
+    BALKS = "balks"
+    WILD_PITCHES = "wild_pitches"
+    BATTERS_FACED = "batters_faced"
+    ERA_PLUS = "era_plus"
+    WHIP = "whip"
+    HITS_PER_9_INNINGS = "hits_per_9_innings"
+    HOME_RUNS_PER_9_INNINGS = "home_runs_per_9_innings"
+    WALKS_PER_9_INNINGS = "walks_per_9_innings"
+    STRIKE_OUTS_PER_9_INNINGS = "strike_outs_per_9_innings"
+    STRIKE_OUT_TO_WALK_RATIO = "strike_out_to_walk_ratio"
