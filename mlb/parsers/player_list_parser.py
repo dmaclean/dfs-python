47273290efe9d96b02757fc77babe776c66938de
Dan MacLean <dmaclean82@gmail.com>
2014-04-28 14:11:37 -0400

    Separated players into active and retired.
    This makes for easier processing when we’re only interested in active
    players.

--- mlb/parsers/player_list_parser.py
+++ mlb/parsers/player_list_parser.py
@@ -6,25 +6,39 @@ import re
 
 
 class PlayerListParser:
-    def __init__(self):
-        self.active_only = False
-        self.player_ids = []
-        self.regex = re.compile('/players/[a-z]/([a-z0-9]+)\.shtml')
+	def __init__(self):
+		self.active_only = False
+		self.active_player_ids = []
+		self.retired_player_ids = []
+		self.odd_player_ids = []
+		self.regex = re.compile('/players/([a-z])/([a-z0-9_\.\']+)\.shtml')
 
-    def parse(self, data):
-        """
-        Perform parsing on the provided data.
-        """
+	def parse(self, data, letter):
+		"""
+		Perform parsing on the provided data.
+		"""
 
-        # Reset the list of player ids
-        self.player_ids = []
+		# Reset the list of player ids
+		self.active_player_ids = []
+		self.retired_player_ids = []
+		self.odd_player_ids = []
 
-        soup = BeautifulSoup(data)
+		soup = BeautifulSoup(data)
 
-        entries = soup.find_all('blockquote')
-        for entry in entries:
-            anchors = entry.find_all('a')
-            for anchor in anchors:
-                if anchor.parent.name == 'b' or not self.active_only:
-                    m = self.regex.match(anchor.attrs['href'])
-                    self.player_ids.append(m.group(1))
+		entries = soup.find_all('blockquote')
+		for entry in entries:
+			anchors = entry.find_all('a')
+			for anchor in anchors:
+				m = self.regex.match(anchor.attrs['href'])
+
+				if m is None:
+					print anchor
+
+				if m.group(1) != letter:
+					self.odd_player_ids.append([m.group(1), m.group(2)])
+				else:
+					# Active player
+					if anchor.parent.name == 'b':
+						self.active_player_ids.append(m.group(2))
+					else:
+						self.retired_player_ids.append(m.group(2))
