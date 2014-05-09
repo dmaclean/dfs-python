fdd0e26b81f8e2671335876979b7c655fc26b232
Dan MacLean <dmaclean82@gmail.com>
2014-05-09 10:28:58 -0400

    Refactored player processing logic into function.
    Refactoring into a function allows us to cleanly process pitchers, too,
    who where being left out previously.

--- mlb/parsers/rotoworld_lineup_scraper.py
+++ mlb/parsers/rotoworld_lineup_scraper.py
@@ -49,26 +49,76 @@ class RotoworldLineupScraper:
 						print "{}".format(player.text)
 						player_name = player.attrs["title"]
 
-					player_data = self.player_manager.players_collection.find_one({'name': player_name}, {"player_id": 1})
-					if player_data is None:
-						logging.critical("Could not find record for {}".format(player_name))
-						continue
-
-					player_id = player_data["player_id"]
-					escaped_player_id = self.lineup_manager.get_id_for_player_name(player_name)
-					if self.lineup_manager.is_processed(escaped_player_id):
-						print "{} already processed.  No scraping necessary.".format(player_name)
-					else:
-						# Found a player.  Let's update their stuff.
-						url = "/players/{}/".format(player_id[0:1])
-						self.bbr_scraper.process_player(player_id, url, active=True)
-
-					# Mark the player as processed (write to the lineup) once their stats have been updated.
-					player_lineup_data = {
+					# player_data = self.player_manager.players_collection.find_one({'name': player_name}, {"player_id": 1})
+					# if player_data is None:
+					# 	logging.critical("Could not find record for {}".format(player_name))
+					# 	continue
+					#
+					# player_id = player_data["player_id"]
+					# escaped_player_id = self.lineup_manager.get_id_for_player_name(player_name)
+					# if self.lineup_manager.is_processed(escaped_player_id):
+					# 	print "{} already processed.  No scraping necessary.".format(player_name)
+					# else:
+					# 	# Found a player.  Let's update their stuff.
+					# 	url = "/players/{}/".format(player_id[0:1])
+					# 	self.bbr_scraper.process_player(player_id, url, active=True)
+					#
+					# # Mark the player as processed (write to the lineup) once their stats have been updated.
+					# player_lineup_data = {
+					# 	"batting_order_position": batting_order_position,
+					#     "opposing_pitcher": home_pitcher if lineup == away_lineup else away_pitcher,
+					#     "team": home_team if lineup == home_lineup else away_team,
+					#     "opponent": home_team if lineup == away_lineup else home_team
+					# }
+					# self.lineup_manager.add_player_to_lineup(escaped_player_id, player_lineup_data)
+					self.process_player(player_name, {
 						"batting_order_position": batting_order_position,
 					    "opposing_pitcher": home_pitcher if lineup == away_lineup else away_pitcher,
-					    "team": home_team if lineup == home_lineup else away_team
-					}
-					self.lineup_manager.add_player_to_lineup(escaped_player_id, player_lineup_data)
+					    "team": home_team if lineup == home_lineup else away_team,
+					    "opponent": home_team if lineup == away_lineup else home_team
+					})
 
 					batting_order_position += 1
+
+			# Process the pitchers
+			self.process_player(away_pitcher, {
+				"batting_order_position": -1,
+			    "opposing_pitcher": home_pitcher,
+			    "team": away_team,
+			    "opponent": home_team
+			})
+
+			self.process_player(home_pitcher, {
+				"batting_order_position": -1,
+			    "opposing_pitcher": away_pitcher,
+			    "team": home_team,
+			    "opponent": away_team
+			})
+
+	def process_player(self, player_name, additional_data):
+		"""
+		Perform data scraping and creation of lineup data for each player.  We also save
+		the player's entry into the day's lineup from here.
+		"""
+		player_data = self.player_manager.players_collection.find_one({'name': player_name}, {"player_id": 1})
+		if player_data is None:
+			logging.critical("Could not find record for {}".format(player_name))
+			return
+
+		player_id = player_data["player_id"]
+		escaped_player_id = self.lineup_manager.get_id_for_player_name(player_name)
+		if self.lineup_manager.is_processed(escaped_player_id):
+			print "{} already processed.  No scraping necessary.".format(player_name)
+		else:
+			# Found a player.  Let's update their stuff.
+			url = "/players/{}/".format(player_id[0:1])
+			self.bbr_scraper.process_player(player_id, url, active=True)
+
+		# Mark the player as processed (write to the lineup) once their stats have been updated.
+		player_lineup_data = {
+			"batting_order_position": additional_data["batting_order_position"],
+		    "opposing_pitcher": additional_data["opposing_pitcher"],
+		    "team": additional_data["team"],
+		    "opponent": additional_data["opponent"]
+		}
+		self.lineup_manager.add_player_to_lineup(escaped_player_id, player_lineup_data)
