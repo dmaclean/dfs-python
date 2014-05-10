61cfa522362c91fa983c1e61ae3eb4e0f10eb519
Dan MacLean <dmaclean82@gmail.com>
2014-05-10 19:20:37 -0400

    Fixed error when name mapping is needed.
    When there’s no match for the provided name, we were not recording the
    remapped name, and later down the line using the original for the
    lineup_manager.get_id_for_player_name() call, which returned None.

--- mlb/parsers/rotoworld_lineup_scraper.py
+++ mlb/parsers/rotoworld_lineup_scraper.py
@@ -1,7 +1,9 @@
 import logging
 import re
 from bs4 import BeautifulSoup
+from mlb.constants.mlb_constants import MLBConstants
 from mlb.models.lineup_manager import LineupManager
+from mlb.models.name_mapping_manager import NameMappingManager
 from mlb.models.player_manager import PlayerManager
 from mlb.utilities.bbr_scraper import BaseballReferenceScraper
 
@@ -14,6 +16,7 @@ class RotoworldLineupScraper:
 		self.player_regex = re.compile("/baseball/player.htm\?id=\d+")
 		self.player_manager = PlayerManager(testing=testing)
 		self.lineup_manager = LineupManager(testing=testing)
+		self.name_mapping_manager = NameMappingManager(testing=testing)
 		self.bbr_scraper = BaseballReferenceScraper()
 		self.bbr_scraper.sleep_time = sleep_time
 
@@ -49,28 +52,6 @@ class RotoworldLineupScraper:
 						print "{}".format(player.text)
 						player_name = player.attrs["title"]
 
-					# player_data = self.player_manager.players_collection.find_one({'name': player_name}, {"player_id": 1})
-					# if player_data is None:
-					# 	logging.critical("Could not find record for {}".format(player_name))
-					# 	continue
-					#
-					# player_id = player_data["player_id"]
-					# escaped_player_id = self.lineup_manager.get_id_for_player_name(player_name)
-					# if self.lineup_manager.is_processed(escaped_player_id):
-					# 	print "{} already processed.  No scraping necessary.".format(player_name)
-					# else:
-					# 	# Found a player.  Let's update their stuff.
-					# 	url = "/players/{}/".format(player_id[0:1])
-					# 	self.bbr_scraper.process_player(player_id, url, active=True)
-					#
-					# # Mark the player as processed (write to the lineup) once their stats have been updated.
-					# player_lineup_data = {
-					# 	"batting_order_position": batting_order_position,
-					#     "opposing_pitcher": home_pitcher if lineup == away_lineup else away_pitcher,
-					#     "team": home_team if lineup == home_lineup else away_team,
-					#     "opponent": home_team if lineup == away_lineup else home_team
-					# }
-					# self.lineup_manager.add_player_to_lineup(escaped_player_id, player_lineup_data)
 					self.process_player(player_name, {
 						"batting_order_position": batting_order_position,
 					    "opposing_pitcher": home_pitcher if lineup == away_lineup else away_pitcher,
@@ -102,8 +83,11 @@ class RotoworldLineupScraper:
 		"""
 		player_data = self.player_manager.players_collection.find_one({'name': player_name}, {"player_id": 1})
 		if player_data is None:
-			logging.critical("Could not find record for {}".format(player_name))
-			return
+			player_name = self.name_mapping_manager.get_player_name(MLBConstants.MONGO_MLB_NAME_MAPPING_ROTOWIRE, MLBConstants.MONGO_MLB_NAME_MAPPING_BBR, player_name)
+			player_data = self.player_manager.players_collection.find_one({'name': player_name}, {"player_id": 1})
+			if player_data is None:
+				logging.critical("Could not find record for {}".format(player_name))
+				return
 
 		player_id = player_data["player_id"]
 		escaped_player_id = self.lineup_manager.get_id_for_player_name(player_name)
