bca0901da0a19d561b6194e2b4afb375c042b021
Dan MacLean <dmaclean82@gmail.com>
2014-05-18 16:32:01 -0400

    Added error handling to stats calculator.

--- mlb/models/stat_calculator.py
+++ mlb/models/stat_calculator.py
@@ -1,3 +1,4 @@
+import logging
 from mlb.constants.mlb_constants import MLBConstants
 
 __author__ = 'dan'
@@ -14,13 +15,16 @@ class StatCalculator:
 		"""
 		wOBA = (0.691*BB + 0.722*HBP + 0.884*1B + 1.257*2B + 1.593*3B + 2.058*HR) / (AB + BB - IBB + SF + HBP)
 		"""
-		singles = data[MLBConstants.HITS] - (data[MLBConstants.DOUBLES] + data[MLBConstants.TRIPLES] + data[MLBConstants.HOME_RUNS])
-
 		try:
+			singles = data[MLBConstants.HITS] - (data[MLBConstants.DOUBLES] + data[MLBConstants.TRIPLES] + data[MLBConstants.HOME_RUNS])
+
 			return (0.691*data[MLBConstants.WALKS] + 0.722*data[MLBConstants.HIT_BY_PITCH] + 0.884*singles +
 			        1.257*data[MLBConstants.DOUBLES] + 1.593*data[MLBConstants.TRIPLES] + 2.058*data[MLBConstants.HOME_RUNS]) / (data[MLBConstants.AT_BATS] + data[MLBConstants.WALKS] - data[MLBConstants.INTENTIONAL_WALKS] + data[MLBConstants.SACRIFICE_FLIES] + data[MLBConstants.HIT_BY_PITCH])
+		except KeyError as err:
+			logging.info("calculate_woba - a required stat is not present. {}".format(str(err)))
+			return 0
 		except ZeroDivisionError as err:
-			print "calculate_woba - division by zero, returning zero."
+			logging.info("calculate_woba - division by zero, returning zero.")
 			return 0
 
 
@@ -28,4 +32,9 @@ class StatCalculator:
 		"""
 		FIP = ((13*HR)+(3*(BB+HBP))-(2*K))/IP + constant
 		"""
-		return ((13*data[MLBConstants.HOME_RUNS])+(3*(data[MLBConstants.WALKS] + data[MLBConstants.HIT_BY_PITCH])-(2*data[MLBConstants.STRIKE_OUTS]))/data[MLBConstants.INNINGS_PITCHED]) + 3.08
+		try:
+			return ((13*data[MLBConstants.HOME_RUNS])+(3*(data[MLBConstants.WALKS] + data[MLBConstants.HIT_BY_PITCH])-(2*data[MLBConstants.STRIKE_OUTS]))/data[MLBConstants.INNINGS_PITCHED]) + 3.08
+		except KeyError, err:
+			return -1
+		except ZeroDivisionError as err:
+			return -1
