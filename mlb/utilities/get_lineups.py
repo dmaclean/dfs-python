56549beb4333fcdd71d16c33a1631ceaed3ef409
Dan MacLean <dmaclean82@gmail.com>
2014-06-15 21:33:00 -0400

    Removed local fetch_data() function
    Using function in MLBUtilities application-wide now.

--- mlb/utilities/get_lineups.py
+++ mlb/utilities/get_lineups.py
@@ -2,7 +2,6 @@ import logging
 import sys
 import time
 from datetime import date, timedelta
-from httplib import HTTPConnection
 
 from mlb.constants.mlb_constants import MLBConstants
 from mlb.models.player_manager import PlayerManager
@@ -26,44 +25,6 @@ class LineupScraper:
 		self.lineup_manager = LineupManager()
 		self.bbr_scraper = BaseballReferenceScraper()
 		self.bbr_scraper.sleep_time = self.sleep_time
-
-	def fetch_data(self, url, log_to_console):
-		"""
-		Makes connection to baseball-reference and downloads data from URL.
-		"""
-		successful = False
-		data = ""
-
-		while not successful:
-			try:
-				conn = HTTPConnection("www.rotowire.com", timeout=5)
-				conn.request("GET", url)
-				resp = conn.getresponse()
-				if resp.status == 301:
-					resp = MLBUtilities.resolve_http_redirect(url, 3)
-
-				content_type = resp.getheader("content-type")
-
-				encoding = None
-				if content_type.find("charset=") > -1:
-					encoding = content_type.split("charset=")[1]
-
-				if log_to_console:
-					print "{} for {}".format(resp.status, url)
-
-				if encoding:
-					data = resp.read().decode(encoding, 'ignore')
-				else:
-					data = resp.read()
-
-				conn.close()
-				successful = True
-			except Exception, err:
-				logging.error("Issue connecting to rotowire ({}).  Retrying in 10 seconds...".format(err))
-				time.sleep(10)
-
-		#time.sleep(5 + (5 * random.random()))
-		return data
 
 	def readCLI(self):
 		for arg in sys.argv:
@@ -155,7 +116,7 @@ class LineupScraper:
 			start = time.time()
 
 			if self.source == "site":
-				data = self.fetch_data("/baseball/daily_lineups.htm", True)
+				data = MLBUtilities.fetch_data("www.rotowire.com", "/baseball/daily_lineups.htm", True)
 			else:
 				data = open(self.source)
 			rotoworld_scraper = RotoworldLineupScraper(sleep_time=self.sleep_time)
@@ -168,6 +129,7 @@ class LineupScraper:
 			time.sleep(60*10)
 
 if __name__ == '__main__':
+	logging.basicConfig(level=logging.INFO)
 	scraper = LineupScraper()
 	scraper.readCLI()
 
